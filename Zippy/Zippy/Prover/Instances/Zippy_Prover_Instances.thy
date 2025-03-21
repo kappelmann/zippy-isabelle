14fb10d0aecbbcf58908ccfed7d93436127d4f52
Kevin Kappelmann <accounts@kappelmann.me>
2025-03-21 16:58:36 +0100

    feat(*) tmp commit before restructure

--- Zippy/Zippy/Prover/Instances/Zippy_Prover_Instances.thy
+++ Zippy/Zippy/Prover/Instances/Zippy_Prover_Instances.thy
@@ -5,12 +5,12 @@ theory Zippy_Prover_Instances
     ML_Unification.ML_Priorities
     Zippy_Prover_Action_Metadata
     Zippy_Prover_Action_Cluster_Metadata
+    Zippy_Prover_Goal_Clusters_Metadata
     Zippy_Prover_Metadata_Data
     Zippy_Prover_Position
     Zippy_Prover_Result_Metadata
 begin
 
-(*TODO: ordering including prios and non-closed subgoals*)
 paragraph \<open>Summary\<close>
 text \<open>The standard Zippy prover instance, using a state monad.\<close>
 
@@ -42,8 +42,8 @@ ML\<open>
   in
     fun halve_prio_co p = update_prio_co (fn _ => P.halve) (P.double p)
     (* val presults_from_tac = Z.presults_from_tac' @{context} *)
-    fun cheat_tac ctxt = resolve_tac ctxt [@{thm cheat}]
-      |> lift_all_goals_focus_tac (K (Zippy_Result_Metadata.metadata RMD.promising))
+    fun cheat_tac prog ctxt = resolve_tac ctxt [@{thm cheat}]
+      |> lift_all_goals_focus_tac (K (Zippy_Result_Metadata.metadata prog))
         T.single_goal_empty_target
     val amd = AMD.metadata (@{binding "test action"}, "action description")
     fun add_tac amd p f tac z =
@@ -51,32 +51,32 @@ ML\<open>
     (* (halve_prio_co p) (cheat_tac mk_rmd ctxt) *)
     val test = Timing.timeit
       (fn _ => (
-      (init_state' mk_gcsd_more mk_gcd_more
+      (init_state' mk_gcd_more
       >>> arr snd
       >>> Down1.move
-      >>> with_state (cheat_tac #> add_tac amd P.HIGH (F.goals [1]))
-      >>> Up4.move >>> Up3.move
-      >>> with_state (cheat_tac #> add_tac amd P.LOW (F.goals [2]))
+      >>> with_state (cheat_tac RMD.unclear #> add_tac amd P.HIGH (F.none))
+      (* >>> Up4.move >>> Up3.move *)
+      (* >>> with_state (cheat_tac RMD.promising #> add_tac amd P.LOW (F.goals [2])) *)
       >>> Up4.move >>> Up3.move >>> Z2.ZM.Down.move
-      >>> with_state (cheat_tac #> add_tac amd P.MEDIUM F.none)
+      >>> with_state (cheat_tac RMD.unclear #> add_tac amd P.MEDIUM F.none)
       >>> Up4.move >>> Up3.move >>> Z2.ZM.Down.move
-      >>> with_state (cheat_tac #> add_tac amd P.MEDIUM F.none)
+      >>> with_state (cheat_tac RMD.unclear #> add_tac amd P.MEDIUM F.none)
       >>> Up4.move >>> Up3.move >>> Z2.ZM.Down.move
-      >>> with_state (cheat_tac #> add_tac amd P.MEDIUM F.none)
+      >>> with_state (cheat_tac RMD.unclear #> add_tac amd P.MEDIUM F.none)
       >>> Up4.move >>> Up3.move >>> Up2.move >>> Z1.ZM.Unzip.move
       >>> repeat_fold_run_max_paction_dfs_halve_prio_depth NONE
       >>> Z1.ZM.Zip.move
-      >>> Down1.move
+      (* >>> Down1.move
       >>> Down2.move >>> Down3.move
       >>> Down4.move
       >>> Down5.move
       >>> Down1.move
-      >>> Down2.move >>> Down3.move
-      >>> top4
-      >>> Down1.move >>> Down2.move >>> Down3.move >>> Down4.move >>> Down5.move
+      >>> Down2.move >>> Down3.move *)
+      (* >>> top1 *)
+      (* >>> Down1.move >>> Down2.move >>> Down3.move >>> Down4.move >>> Down5.move *)
       (* >>> Down4.move *)
       (* >>> Down5.move  *)
-      (* >>> with_state pretty_gc *)
+      (* >>> with_state pretty_gcs *)
       (* >>> pretty_actiona *)
       (* >>> \<^imap>\<open>\<open>{i}\<close> => \<open>Down{i}.move\<close> where sep = ">>>" and stop = 2\<close> *)
       (* >>> Up5.move *)
@@ -84,7 +84,8 @@ ML\<open>
       (* >>> pretty_actionc *)
       (* >>> top4 *)
       (* >>> Z1.ZM.Zip.move *)
-      (* >>> finish_gclusters_oldest_first @{context} >>> arr Seq.list_of *)
+      (* >>> finish_gclusters_oldest_first @{context} >>> arr (Seq.list_of) *)
+      >>> finish_promising_gclusters_oldest_first @{context} >>> arr (Seq.list_of)
       (* >>> up *)
       (* >>> L.get (L.lens_snd (L.id ())) *)
       (* >>> finish_gclusters_oldest_first @{context} *)
@@ -94,7 +95,20 @@ ML\<open>
   end
 \<close>
 
-ML\<open>
-  val a = THEN
-\<close>
+(*
+
+GCS
+|       |
+GC      GC
+|   |    |
+AC  AC   AC
+| |  |    |
+A A  A    A
+|    |    |
+AA   AA   AA
+|    |    |
+GCS  GCS  GCS
+
+*)
+
 end
